e85b99c138534d0576949625557392cd3857ba06
Dmitry Pervoukhin <dvperv@gmail.com>
2023-01-19 23:25:22 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	//router.Static("/assets", "./assets")
+	//router.StaticFS("/more_static", http.Dir("my_file_system"))
+	router.StaticFile("/more_static/1", "./more_static/1.txt")
+
+	// Listen and serve on 0.0.0.0:8080
+	router.Run(":8085")
+}
