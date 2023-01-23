685567833a532ee0542cfcaa3b6af3b34679542b
dvperv <dvperv@gmail.com>
2023-01-23 12:48:45 +0300

    test files added

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ func main() {
 	//router.Static("/assets", "./assets")
 	//router.StaticFS("/more_static", http.Dir("my_file_system"))
 	router.StaticFile("/more_static/1", "./more_static/1.txt")
+	router.StaticFile("/more_static/1", "./more_static/2.txt")
+	router.StaticFile("/more_static/1", "./more_static/3.txt")
 
 	// Listen and serve on 0.0.0.0:8080
 	router.Run(":8085")
